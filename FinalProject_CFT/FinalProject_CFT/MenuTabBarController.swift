80da94e947a8bd1ffffb08afebb8774083630470
Vyacheslav <guzxaroh@gmail.com>
2024-06-11 14:30:26 +0700

    init commit

---
+++ FinalProject_CFT/FinalProject_CFT/MenuTabBarController.swift
@@ -0,0 +1,14 @@
+//
+//  MenuTabBarController.swift
+//  FinalProject_CFT
+//
+//  Created by Vyacheslav on 11.06.2024.
+//
+
+import UIKit
+
+class MenuTabBarController: UITabBarController {
+    
+    
+    
+}
